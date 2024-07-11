8e8b937a00381dcd301008ee5a740cc0236ccf53
rushmi0 <gcalku@gmail.com>
2024-07-11 20:11:28 +0700

    .

--- run_jvm.sh
+++ run_jvm.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# เริ่มต้นตรวจสอบว่า Gradle สามารถคอมไพล์โปรเจคได้โดยไม่ต้องรันอีกครั้ง
 if [ ! -f "build/libs/fenrir-s-0.1-all-optimized.jar" ]; then
+    echo "Building project..."
     ./gradlew build
+    if [ $? -ne 0 ]; then
+        echo "Error: Gradle build failed."
+        exit 1
+    fi
 fi
 
+# รันแอปพลิเคชัน Java จาก JAR ที่คอมไพล์แล้ว
+echo "Starting application..."
 java -jar build/libs/fenrir-s-0.1-all-optimized.jar
