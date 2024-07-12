f082e183daf4fd0ac7b31787101bfc1a478c00a7
rushmi0 <gcalku@gmail.com>
2024-07-12 19:10:00 +0700

    เพิ่ม banner

--- src/main/kotlin/org/fenrirs/Application.kt
+++ src/main/kotlin/org/fenrirs/Application.kt
@@ -1,11 +1,24 @@
 package org.fenrirs
 
-import io.micronaut.runtime.Micronaut.run
+import io.micronaut.runtime.Micronaut
 import org.fenrirs.relay.service.ProfileSync
 
 fun main(args: Array<String>) {
 
-    val relay = run(*args)
+    println("""
+    ___________                 .__                         
+    \_   _____/___   ___________|__|______            ______
+     |    __)/ __ \ /    \_  __ \  \_  __ \  ______  /  ___/
+     |     \\  ___/|   |  \  | \/  ||  | \/ /_____/  \___ \ 
+     \___  / \___  >___|  /__|  |__||__|            /____  >
+         \/      \/     \/                               \/ 
+         version 0.1.0 by rushmi0
+
+    """.trimIndent())
+    val relay = Micronaut.build()
+        .args(*args)
+        .banner(false)
+        .start()
 
     if (relay.isRunning) {
         relay.getBean(ProfileSync::class.java).sync()
